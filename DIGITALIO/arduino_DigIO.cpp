60d541367c77c543f080d7751f84c8578d698ace
Pablo Quiroga <pablodie@gmail.com>
2016-01-08 16:34:52 -0400

    Some changes

--- DIGITALIO/arduino_DigIO.cpp
+++ DIGITALIO/arduino_DigIO.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 
-class DigitalIO {
+class digitalIO {
 	
-	int	 _pin;
-	string _mode;
+	int	pin_num;
+	int state;
+	int mode;
+	std::vector<int> pin;
 
+	void set_pin(*pin, int pin_num)
+	{	
+		pin->pin_num = pin_num;
+		pin->state = LOW;
+	}
 
-	int pinMode()
+	void pinMode(*pin, mode)
 	{
-		_pin = 
-		_mode = 
+		pin->mode = mode;
+	}
+
+	void digitalIO_write(*pin, state)
+	{
+		__ESBMC_assert(pin->mode == OUTPUT);
+		pin->cur_state = state;
+	}
+
+	int digitalIO_read(*pin)
+	{
+		__ESBMC_assert(pin.mode == INPUT );
+		int value;
+		__ESBMC_assume (value ==  LOW || value == HIGH);
+		return value;	
+	}
 
--- EEPROM/arduino_EEPROM.cpp
+++ EEPROM/arduino_EEPROM.cpp
@@ -1,24 +1,27 @@
 #inclue <iostream>
 
-class EEPROM_write {
+class EEPROM {
+	public: 
+		int address, value, size;
+		std::vector<int> byte;
 
-	int address , value;
+		int EEPROM_write( *byte,address, size, value)
+		{
+			__ESBMC_assert (address < size);
+			__ESBMC_assert (value <= 255);
+			return byte[address] = value;
+		}	
 
-	eeprom_arduino.write(address, value);
+		int EEPROM_read (*byte, address, value, size)
+		{
+			__ESBMC_assert (address,value);
+			return byte[address];
+		}
+
+		int EEPROM_update(*byte, address, value, size)
+		{
+			__ESBMC_assert(address != value) ;
+			return byte[address];
+		}
+
 }
-
-class EEPROM_read {
-	int arg;
-
-	eeprom_arduino.read(arg);
-}
-
-class EEPROM_update {
-	int address, value;
-
-	eeprom_update.update(address, value);
-}
-
-
-
-
