303188fc761943acb35c096b9d5a2fd04ccf6878
Pablo Quiroga <pablodie@gmail.com>
2016-01-18 15:20:44 -0400

    upload

--- app1/button_test.cpp
+++ app1/button_test.cpp
@@ -6,17 +6,19 @@
 // Set your serial console to 19200
 // Written by Will O'Brien, based on various others works.
 
+using namespace std;
 ardDigitalIO digitalIO;
 ardTime aTime;
 
 #define ROWS 2
 #define COLS 4
 
-const byte buttonWrite[2] = {2, 3}; //Pins for the Vin of the buttons
-const byte buttonRead[4] = {6, 7, 8, 9}; //Pins for reading the state of the buttons
+int buttonWrite[2] = {2, 3}; //Pins for the Vin of the buttons
+int  buttonRead[4] = {6, 7, 8, 9}; //Pins for reading the state of the buttons
 bool pressed[ROWS][COLS] = {  0};
-  const byte *lock_pin = 4;
+int* lock_pin;
 bool lockState = 0;
+
 
 void setup(){
   //Serial.begin(19200);
@@ -36,28 +38,28 @@ void setup(){
 
 
 void loop(){
-  Serial.print(".");
-  for(byte r = 0; r < ROWS; ++r){
+//  Serial.print(".");
+  for(int r = 0; r < ROWS; ++r){
 
     digitalIO.digitalWrite(buttonWrite[r], HIGH);
-    for(byte c = 0; c < COLS; ++c){
-      if(pressed[r][c] != digitalIO.digitalRead(buttonRead[c])){
-        pressed[r][c] = digitalIO.digitalRead(buttonRead[c]);
+    for(int  c = 0; c < COLS; ++c){
+      if(pressed[r][c] != digitalIO.digitalRead(*buttonRead[c])){
+        pressed[r][c] = digitalIO.digitalRead(*buttonRead[c]);
         if(pressed[r][c]){
           on_press(r, c);
         } 
       } 
     }
     aTime.delay(5);
-    digitalIO.digitalWrite(buttonWrite[r], LOW); 
+    digitalIO.digitalWrite(&buttonWrite[r], LOW); 
   }
  aTime. delay(10);
 }
 
-void on_press(byte r, byte c){
-  Serial.print(r, DEC);
-  Serial.print(", ");
-  Serial.println(c, DEC);
+void on_press(int r, int c){
+ // Serial.print(r, DEC);
+ // Serial.print(", ");
+  //Serial.println(c, DEC);
   set_lock();
 }
 
@@ -69,13 +71,13 @@ void set_lock(){
    lock(); 
 }
 void unlock(){
-  Serial.print("Unlock door");
-  digitalIO.digitalWrite(lock_pin, HIGH);
+ // Serial.print("Unlock door");
+  digitalIO.digitalWrite(&lock_pin, HIGH);
   lockState = !lockState;
 }
 void lock(){
-  Serial.print("Lock door");
-  digitalWrite(lock_pin, LOW);
+ // Serial.print("Lock door");
+  digitalIO.digitalWrite(&lock_pin, LOW);
   lockState = !lockState;
 }
 
