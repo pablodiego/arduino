7023fb2b7026212946891e2d65970d4b0fb85ef1
Pablo Quiroga <pablodie@gmail.com>
2016-02-14 19:21:48 -0500

    DigitalWrite works correctly

--- libraries/arduino.h
+++ libraries/arduino.h
@@ -11,85 +11,114 @@
 #define BOUNDS 2
 #define kMemorySize 255
 
+#include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class ArduinoPin{
+
 public:
-
-int current_state;
-int mode;
-
 	
-	/*ArduinoPin(int pin){
-		numPin = pin;
-	};*/
+	int rpin;
+	int mode;
+	int current_state;
 
-		int getPin(){
-			return pin;
-		}
-		
+	ArduinoPin(){}
+
 	void setPin(int pin){
-		this->pin=pin;
+		this->pin = pin;
 	}
 
 	void setMode (int mode){
-		this->mode = mode;
+		mode = mode;
 	}
 
 	void setState (int state){
 		current_state = state;
 	}
 
+	int& getPin(){
+		return pin;
+	}
+
+	int& getMode(){
+		return mode;
+	}
+
+	int& getState(){
+		return current_state;
+	}
+
 private:
 	int pin;
 };
 
-static vector<ArduinoPin * > pin;
+
+ArduinoPin *vPin = new ArduinoPin[1];
+int size =0;
+
 
 int pinMode (int pin, int mode){
-		//ArduinoPin numPin(pin);
-		//numPin.setMode(mode);
-	}
 
+	vPin[size].setPin(pin);
+	vPin[size].setMode(mode);
+	vPin[size].setState(LOW);
+	size++;
+
+	}
 
 int digitalWrite (int pin, int state)
 {
-	int cur_state;
-	int mode;
-	//int* numPin;
+
+	int j= -1;
+
+	for (int i=0; i < size; i++){
+		if (vPin[i].getPin() == pin){
+			j = i;
+		}
+	} 
 	
-	//__ESBMC_assert(numPin[mode] == OUTPUT, "Error");
-	//numPin[cur_state] = LOW;
+	__ESBMC_assert(j != -1, "Error Cabuloso");
+	int uPin = vPin[j].getMode();
+	__ESBMC_assert(uPin == OUTPUT, "Error");
+	vPin[j].setState(state);
 }
 
 int digitalRead (int pin)
 {
-	int mode;
-	//int* numPin;
 	
-	//__ESBMC_assert(numPin[mode] == INPUT, "Error"  );
-	int value;
-	__ESBMC_assume (value ==  LOW || value == HIGH);
-	return value;
+	int j= -1;
+
+	for (int i=0; i < size; i++){
+		if (vPin[i].getPin() == pin){
+			j = i;
+		}
+	}
+
+	int uPin = vPin[j].getState();
+
+	__ESBMC_assume( uPin ==  LOW || uPin == HIGH);
+	return uPin;
 }
 
 int analogRead (int pin)
 {
-	int numPin;
-	__ESBMC_assume(numPin >=0 && numPin <=1023);
-	return numPin;
+	__ESBMC_assume(pin >=0 && pin <=1023);
+	return pin;
 }
 
-int analogWrite (int numPin, int  value)
+int analogWrite (int pin, int value)
 {
-	__ESBMC_assert (value >= 0 && value <= 255, "Error" );
+	int rvalue;
+	rvalue = value;
+	__ESBMC_assert (rvalue >= 0 && rvalue <= 255, "Error" );
 }
 
-long delay(long time)
+void delay(long time)
 {
-	long time;
-	__ESBMC_assume (time > 0);
+	
+	__ESBMC_assert (time > 0, " Error");
 }
 
 long millis()
@@ -97,6 +126,11 @@ long millis()
 	long time;
 	__ESBMC_assume (time > 0);
 	return time;
+}
+
+int searchPin (int pin){
+
+	//for (int i =0; i <= kMemorySize , i++) vPin.push_back(i);
 }
 
 #endif
